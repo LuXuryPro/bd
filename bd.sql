6a4f2a1d4b9303dc31ea2400d197226220f544dc
novenau <user@example.com>
2015-05-15 19:37:48 +0200

    pozostalo zrobic selecty

--- bd.sql
+++ bd.sql
@@ -10,6 +10,7 @@ ALTER TABLE TWORZENIE_GRY DROP CONSTRAINT TWORZENIE_GRY_fk2;
 ALTER TABLE TWORZENIE_GRY DROP CONSTRAINT TWORZENIE_GRY_fk3;
 ALTER TABLE KOMENTARZ_DO_RECENZJI DROP CONSTRAINT KOMENTARZ_DO_RECENZJI_fk1;
 ALTER TABLE KOMENTARZ_DO_RECENZJI DROP CONSTRAINT KOMENTARZ_DO_RECENZJI_fk2;
+ALTER TABLE KOMENTARZ_DO_RECENZJI DROP CONSTRAINT KOMENTARZ_DO_RECENZJI_fk3;
 ALTER TABLE RECENZJA DROP CONSTRAINT recenzja_fk1;
 ALTER TABLE RECENZJA DROP CONSTRAINT recenzja_fk2;
 ALTER TABLE RECENZJA DROP CONSTRAINT recenzja_fk3;
@@ -72,7 +73,7 @@ DROP TRIGGER UZYTKOWNIK_seq_trg;
 create table WYDAWCA(
   wydawca_id NUMBER,
   nazwa VARCHAR2(200)
-  
+
 );
 
 CREATE SEQUENCE WYDAWCA_seq;
@@ -210,8 +211,8 @@ create table TWORZENIE_GRY (
   gra_id NUMBER,
   platforma_id NUMBER,
   studio_deweloperskie_id NUMBER,
-  czas_powstawania NUMBER,
-  budzet NUMBER
+  czas_powstawania NUMBER(*,1),
+  budzet NUMBER(*,1)
 );
 --------------------------------------------------------------------------------
 
@@ -240,6 +241,7 @@ create table KOMENTARZ_DO_RECENZJI (
   komentarz_do_recenzji_id NUMBER,
   gra_id NUMBER,
   recenzja_id NUMBER,
+  uzytkownik_id NUMBER,
   data DATE,
   tekst VARCHAR2(200)
 );
@@ -282,8 +284,9 @@ ALTER TABLE RECENZJA ADD CONSTRAINT recenzja_fk3 FOREIGN KEY (autor_id) REFERENC
 ALTER TABLE KOMENTARZ_DO_RECENZJI ADD CONSTRAINT KOMENTARZ_DO_RECENZJI_pk PRIMARY KEY (komentarz_do_recenzji_id);
 ALTER TABLE KOMENTARZ_DO_RECENZJI ADD CONSTRAINT  KOMENTARZ_DO_RECENZJI_fk1 FOREIGN KEY (gra_id) REFERENCES GRA(gra_id);
 ALTER TABLE KOMENTARZ_DO_RECENZJI ADD CONSTRAINT  KOMENTARZ_DO_RECENZJI_fk2 FOREIGN KEY (recenzja_id) REFERENCES RECENZJA(recenzja_id);
+ALTER TABLE KOMENTARZ_DO_RECENZJI ADD CONSTRAINT  KOMENTARZ_DO_RECENZJI_fk3 FOREIGN KEY (uzytkownik_id) REFERENCES UZYTKOWNIK(uzytkownik_id);
 
-create or replace FUNCTION GENERATOR_DATY(POCZATEK_ROK in NUMBER, KONIEC_ROK IN NUMBER) 
+create or replace FUNCTION GENERATOR_DATY(POCZATEK_ROK in NUMBER, KONIEC_ROK IN NUMBER)
 RETURN DATE IS
 	y NUMBER(4);
   d NUMBER(4);
@@ -381,10 +384,141 @@ END GENERATOR_WYDANIE_GRY;
 
 EXECUTE  GENERATOR_WYDANIE_GRY(100);
 
-select GRA.NAZWA,WYDAWCA.NAZWA as NAZWA_WYDAWCY,PLATFORMA.NAZWA as NAZWA_PLATFORMY,WYDANIE_GRY.DATA as DATA_WYDANIA
-from GRA join WYDANIE_GRY on GRA.gra_id = WYDANIE_GRY.gra_id
+
+create or replace PROCEDURE GENERATOR_KOMENTARZ_DO_GRY(ILOSC in NUMBER) IS
+BEGIN
+	FOR i IN 1..ILOSC LOOP
+		INSERT INTO KOMENTARZ_DO_GRY VALUES (NULL,
+    (select gra_id from (select gra_id from GRA order by dbms_random.value) where rownum = 1),
+    (select UZYTKOWNIK_ID from (select UZYTKOWNIK_ID from UZYTKOWNIK order by dbms_random.value) where rownum = 1),
+    GENERATOR_DATY(1990,2015),
+    dbms_random.string('A', DBMS_RANDOM.VALUE(10,200))
+    );
+	END LOOP;
+END GENERATOR_KOMENTARZ_DO_GRY;
+/
+
+EXECUTE  GENERATOR_KOMENTARZ_DO_GRY(100);
+
+create or replace PROCEDURE GENERATOR_PORADNIK_DO_GRY(ILOSC in NUMBER) IS
+TYPE TABSTR IS TABLE OF VARCHAR2(250);
+tags TABSTR;
+BEGIN
+  tags := TABSTR('nowosc','hit','nowy poradnik','kompletny poradnik','w trakcie tworzenia','darmowy','płatny');
+	FOR i IN 1..ILOSC LOOP
+		INSERT INTO PORADNIK_DO_GRY VALUES (NULL,
+    (select UZYTKOWNIK_ID from (select UZYTKOWNIK_ID from UZYTKOWNIK order by dbms_random.value) where rownum = 1),
+    (select gra_id from (select gra_id from GRA order by dbms_random.value) where rownum = 1),
+    (select platforma_id from (select platforma_id from PLATFORMA order by dbms_random.value) where rownum = 1),
+    dbms_random.string('A', DBMS_RANDOM.VALUE(10,200)),
+    tags(1+mod(i,7)) || ',' || tags(1+mod(i+1,7)) || ',' || tags(1+mod(i+2,7))
+    );
+	END LOOP;
+END GENERATOR_PORADNIK_DO_GRY;
+/
+
+EXECUTE  GENERATOR_PORADNIK_DO_GRY(100);
+
+
+create or replace PROCEDURE GENERATOR_TWORZENIE_GRY(ILOSC in NUMBER) IS
+BEGIN
+	FOR i IN 1..ILOSC LOOP
+  <<UP>>
+  BEGIN
+		INSERT INTO TWORZENIE_GRY VALUES (
+    (select gra_id from (select gra_id from GRA order by dbms_random.value) where rownum = 1),
+    (select platforma_id from (select platforma_id from PLATFORMA order by dbms_random.value) where rownum = 1),
+    (select STUDIO_DEWELOPERSKIE_id from (select STUDIO_DEWELOPERSKIE_id from STUDIO_DEWELOPERSKIE order by dbms_random.value) where rownum = 1),
+    DBMS_RANDOM.VALUE(1,5),
+    DBMS_RANDOM.VALUE(10000,500000)
+    );
+    EXCEPTION
+      WHEN DUP_VAL_ON_INDEX THEN
+      goto UP;
+    END;
+	END LOOP;
+END GENERATOR_TWORZENIE_GRY;
+/
+
+EXECUTE  GENERATOR_TWORZENIE_GRY(100);
+
+create or replace PROCEDURE GENERATOR_RECENZJA(ILOSC in NUMBER) IS
+TYPE TABSTR IS TABLE OF VARCHAR2(250);
+tags TABSTR;
+BEGIN
+  tags := TABSTR('dobra ocena','slaba ocena','autor poleca','nowa','w trakcie pisania');
+	FOR i IN 1..ILOSC LOOP
+		INSERT INTO RECENZJA VALUES (NULL,
+    (select gra_id from (select gra_id from GRA order by dbms_random.value) where rownum = 1),
+    (select platforma_id from (select platforma_id from PLATFORMA order by dbms_random.value) where rownum = 1),
+    (select uzytkownik_id from (select uzytkownik_id from UZYTKOWNIK order by dbms_random.value) where rownum = 1),
+    dbms_random.string('A', DBMS_RANDOM.VALUE(10,20)),
+    dbms_random.string('A', DBMS_RANDOM.VALUE(10,200)),
+    tags(1+mod(i,5)) || ',' || tags(1+mod(i+1,5)) || ',' || tags(1+mod(i+2,5))
+    );
+	END LOOP;
+END GENERATOR_RECENZJA;
+/
+
+EXECUTE GENERATOR_RECENZJA(100);
+
+
+
+
+
+create or replace PROCEDURE GEN_KOMENTARZ_DO_RECENZJI(ILOSC in NUMBER) IS
+BEGIN
+	FOR i IN 1..ILOSC LOOP
+		INSERT INTO KOMENTARZ_DO_RECENZJI VALUES (NULL,
+    (select gra_id from (select gra_id from GRA order by dbms_random.value) where rownum = 1),
+    (select recenzja_id from (select recenzja_id from RECENZJA order by dbms_random.value) where rownum = 1),
+    (select UZYTKOWNIK_ID from (select UZYTKOWNIK_ID from UZYTKOWNIK order by dbms_random.value) where rownum = 1),
+    GENERATOR_DATY(1990,2015),
+    dbms_random.string('A', DBMS_RANDOM.VALUE(10,200))
+    );
+	END LOOP;
+END GEN_KOMENTARZ_DO_RECENZJI;
+/
+
+EXECUTE  GEN_KOMENTARZ_DO_RECENZJI(100);
+
+commit;
+
+--Wyświetl wszystkie gry
+--podaj ich nazwę, nazwę wydawcy, nazwę platformy i datę wydania
+--posortuj wg daty wydania
+select GRA.NAZWA,WYDAWCA.NAZWA as NAZWA_WYDAWCY,PLATFORMA.NAZWA as NAZWA_PLATFORMY,WYDANIE_GRY.DATA as DATA_WYDANIA from GRA
+join WYDANIE_GRY on GRA.gra_id = WYDANIE_GRY.gra_id
 join WYDAWCA on WYDANIE_GRY.wydawca_id = WYDAWCA.WYDAWCA_ID
 join PLATFORMA on WYDANIE_GRY.platforma_id = PLATFORMA.platforma_id
 order by WYDANIE_GRY.DATA;
 
-commit;
+
+--Wyświetl wszystkie komentarze do gier urzytkownika o id = 1
+select KOMENTARZ_DO_GRY.TEKST as TEKST_KOMENTARZA
+from KOMENTARZ_DO_GRY where UZYTKOWNIK_ID = 1;
+
+--wyświetl gry z tagiem remake
+select GRA.NAZWA,WYDAWCA.NAZWA as NAZWA_WYDAWCY,PLATFORMA.NAZWA as NAZWA_PLATFORMY,WYDANIE_GRY.DATA as DATA_WYDANIA,
+GRA.TAGI AS TAGI from GRA
+join WYDANIE_GRY on GRA.gra_id = WYDANIE_GRY.gra_id
+join WYDAWCA on WYDANIE_GRY.wydawca_id = WYDAWCA.WYDAWCA_ID
+join PLATFORMA on WYDANIE_GRY.platforma_id = PLATFORMA.platforma_id where GRA.TAGI like '%remake%'
+order by WYDANIE_GRY.DATA;
+
+--wyświetl najchętniej komentowane recenzje
+select RECENZJA.TYTUL as TYTUL_RECENZJI, UZYTKOWNIK.NAZWA as NAZWA_AUTORA, ILOSC_KOMENTARZY from
+(select RECENZJA.RECENZJA_ID as IDR, count(*) as ILOSC_KOMENTARZY
+from RECENZJA join KOMENTARZ_DO_RECENZJI on RECENZJA.RECENZJA_ID = KOMENTARZ_DO_RECENZJI.RECENZJA_ID
+group by RECENZJA.RECENZJA_ID)
+join RECENZJA on RECENZJA.RECENZJA_ID = IDR
+join UZYTKOWNIK on UZYTKOWNIK.UZYTKOWNIK_ID = RECENZJA.AUTOR_ID
+order by (ILOSC_KOMENTARZY) DESC;
+
+--wyswietl komentarze do najchetniej komentowanej recenzji
+select KOMENTARZ_DO_RECENZJI.TEKST as TEKST_KOMENTARZA
+from KOMENTARZ_DO_RECENZJI where RECENZJA_ID =
+(select IDR from (select RECENZJA.RECENZJA_ID as IDR, count(*) as ILOSC_KOMENTARZY
+from RECENZJA join KOMENTARZ_DO_RECENZJI on RECENZJA.RECENZJA_ID = KOMENTARZ_DO_RECENZJI.RECENZJA_ID
+group by RECENZJA.RECENZJA_ID
+order by (ILOSC_KOMENTARZY) DESC) where rownum = 1);
